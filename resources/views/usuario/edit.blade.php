b23b6534b0c4cbfab6a587019683e70c56cf73bb
Francisco Javier Barrera Romero <125419028+Javier0015@users.noreply.github.com>
2023-10-14 19:23:50 -0600

    Update edit.blade.php

--- resources/views/usuario/edit.blade.php
+++ resources/views/usuario/edit.blade.php
@@ -1,7 +1,10 @@
-<link rel="stylesheet" href="assets/css/style_login.css">
+
 
 @extends('layouts.app')
 @section('content')
+
+<link rel="stylesheet" href="assets/css/style_login.css">
+
 <form action="{{ url('/usuario/'.$usuario->id ) }}" method="post">
 @csrf
 {{ method_field('PATCH') }}
